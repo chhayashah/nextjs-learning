a22eaa62c08b0172b61ddede21c5b9367f22a51f
chhaya shah <chhayashah9754@gmail.com>
2026-07-16 05:22:46 +0530

    Image component

--- app/(users)/service/page.tsx
+++ app/(users)/service/page.tsx
@@ -1,6 +1,26 @@
+import Image from "next/image";
 import style from "./service.module.css";
 
 const Service = () => {
     return <h1 className={style.common_heading}>hello service</h1>;
+    <section className=" ">
+      <div className="max-w-4xl mx-auto px-6">
+            <h2 className="text-3xl font-bold text-center mb-12 text-gray-800">
+                Our Team
+            </h2>
+            <div className="grid grid-cols-3 gap-8">
+                <div className="bg-white rounded-lg shadow-md p-6 hover:shadow-lg hover:-translate-y-2 transition-all duration-300">
+                    <div className="w-24 h-24 bg-blue-200 rounded-full mx-auto mb-4 flex items-center justify-center">
+                        <Image src="/chhayaprofile.jpg" width={500} height={500} alt="chhaya 
+                        className="w-full h-full rounded-full" />
+                    </div>
+                    <h3 className="text-lg font-semibold text-center text-gray-800">Chhaya Shah</h3>
+                    <p className="text-sm text-gray-600 text-center mt-2">software developer</p>
+                    <p className="text-xs text-gray-500 text-center mt-1">React & TypeScript</p>
+                </div>
+
+            </div>
+      </div>
+    </section>;
 }
 export default Service;
